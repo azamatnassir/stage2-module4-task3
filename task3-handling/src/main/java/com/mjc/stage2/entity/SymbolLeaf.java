b68ef9e06fe58687902de6393586c94c9e13b726
azamatnassir <azamatnsr@gmail.com>
2023-08-13 02:44:57 +0600

    commit

--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,33 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+public class SymbolLeaf extends AbstractTextComponent {
 
     private char value;
+
+    public SymbolLeaf(TextComponentType componentType, char c) {
+        super(componentType);
+        this.value = c;
+    }
+
+    @Override
+    public String operation() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getSize() {
+        return 1;
+    }
 
     // Write your code here!
 }
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,18 @@
 package com.mjc.stage2.parser;
 
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
 
-public class WordParser {
-
-    // Write your code here!
-
+public class WordParser extends AbstractTextParser {
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        if (abstractTextComponent.getComponentType() == TextComponentType.WORD) {
+            for (char c : string.toCharArray()) {
+                abstractTextComponent.add(new SymbolLeaf(TextComponentType.SYMBOL, c));
+            }
+        } else if (nextParser != null) {
+            nextParser.parse(abstractTextComponent, string);
+        }
+    }
 }
